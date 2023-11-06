a377212fa6fba7a8006af48ea5a96c8b4250eb4b
ubay1 <tomdelonge976@gmail.com>
2023-11-07 05:32:08 +0700

    fixing code in markdown

--- src/lib/components/blog/CopyCode.svelte
+++ src/lib/components/blog/CopyCode.svelte
@@ -2,30 +2,43 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		// will add a children to any <pre> element with class language-*
 		let pres: HTMLCollection = document.getElementsByTagName('pre');
 		for (let _ of pres) {
 			const pre = _ as HTMLPreElement;
 
 			const text = pre.innerText;
+
+			let divHeader = document.createElement('div');
+			divHeader.className = 'bg-black h-15 absolute top-0 w-full flex items-center justify-between';
+
+			const dataTitle = pre.getAttribute('data-code-title');
+			let title = document.createElement('div');
+			title.innerText = dataTitle as string;
+			title.className = 'ml-4 text-sm';
+
 			let copyButton = document.createElement('button');
-			copyButton.className = 'absolute top-2 right-2 btn-copy text-white';
+			copyButton.className = 'h-10 w-10 mr-4 flex items-center justify-center btn-copy text-white';
 			copyButton.innerHTML = `<div class='i-octicon-copy-16'></div>`;
 
 			copyButton.addEventListener('click', () => {
 				navigator.clipboard.writeText(text);
+
 				copyButton.disabled = true;
 				copyButton.className =
-					'absolute top-2 right-2 btn-copy text-white p-1 disabled:bg-orange-5 disabled:opacity-100';
+					'h-10 w-10 mr-4 flex items-center justify-center btn-copy text-white p-1 disabled:bg-orange-5 disabled:opacity-100';
 				copyButton.innerHTML = `<div class='i-octicon-check-24 p-1'></div>`;
 
 				setTimeout(function () {
-					copyButton.className = 'absolute top-2 right-2 btn-copy text-white';
+					copyButton.className =
+						'h-10 w-10 mr-4 flex items-center justify-center btn-copy text-white';
 					copyButton.disabled = false;
 					copyButton.innerHTML = `<div class='i-octicon-copy-16'></div>`;
 				}, 2000);
 			});
-			pre.appendChild(copyButton);
+
+			pre.appendChild(divHeader);
+			divHeader.appendChild(title);
+			divHeader.appendChild(copyButton);
 		}
 	});
 </script>
