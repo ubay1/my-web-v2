c0e5e9c16b04b62a2caa468259cc5b106f71a38f
ubay1 <tomdelonge976@gmail.com>
2024-08-19 07:39:51 +0700

    tes adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
   output: 'server',
-  integrations: [
-    tailwind(),
-    react({
-      include: ['**/react/*'],
-    }),
-  ],
-})
+  integrations: [tailwind(), react({
+    include: ['**/react/*']
+  })],
+  adapter: vercel()
+});
