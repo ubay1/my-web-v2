e172b0180065fcaa0727f0c6bce18645207ad80a
ubay1 <tomdelonge976@gmail.com>
2024-11-19 03:59:27 +0700

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import {
   transformerRenderWhitespace,
   transformerRemoveNotationEscape,
 } from '@shikijs/transformers'
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,6 +42,47 @@ export default defineConfig({
     tailwind(),
     react({
       include: ['**/react/*'],
+    }),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Astro PWA',
+        short_name: 'Astro PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
     }),
   ],
   adapter: vercel(),
