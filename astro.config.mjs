8606e667275d7bee2c5e610e41d54fbea34ef49e
ubay1 <tomdelonge976@gmail.com>
2025-11-12 09:18:44 +0700

    change adapter to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify'
 import {
   transformerNotationHighlight,
   transformerMetaHighlight,
@@ -148,5 +148,5 @@ export default defineConfig({
     // }),
     astroI18next(),
   ],
-  adapter: vercel(),
+  adapter: netlify(),
 })
